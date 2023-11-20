0de5006a1514c83bd8479dfdf34e4ac58ef99627
meeky <britanico.me@gmail.com>
2023-11-20 00:11:47 -0500

    fixed domain ID userRepo

--- services/user/src/main/java/com/hotel/user/UserRepository.java
+++ services/user/src/main/java/com/hotel/user/UserRepository.java
@@ -2,7 +2,7 @@ package com.hotel.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     // User addFoodLogEntry(String email, String date, String foodName, int quantity);
 }
